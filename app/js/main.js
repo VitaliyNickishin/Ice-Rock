d61e804f90ed639e64ebb0a2993fdd40b2b85e2b
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-19 00:00:16 +0300

    create page-power

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
 
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
-
-}); 
-
-
+function fun1() {
+ var rng=document.getElementById('myRange'); //rng - это ползунок
+var i1=document.getElementById('i1'); // i1 - input
+i1.value=rng.value;
+}s
