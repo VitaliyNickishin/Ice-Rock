76de5077a57f27c9e3ab61bb552cf1b24b8ffd5d
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-19 03:06:19 +0300

    create page-power-footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,8 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+let rng = document.getElementById('myRange');
+let power = document.getElementById('power');
+let cost = document.getElementById('cost');
+ rng.oninput = function calc() {
+ power.value=rng.value;
+ cost.value=power.value*3.30;
 }
-
-function fun1() {
- var rng=document.getElementById('myRange'); //rng - это ползунок
-var i1=document.getElementById('i1'); // i1 - input
-i1.value=rng.value;
-}s
