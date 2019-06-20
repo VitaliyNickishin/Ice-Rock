538149377cee2706fcb47680d5604241c6ae920b
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-20 17:13:28 +0300

    add branch for  slider powerange

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,18 @@ let cost = document.getElementById('cost');
  power.value=rng.value;
  cost.value=power.value*3.30;
 }
+
+/*из библ. Powerange вставляем ползунок*/
+ //находим наш класс в ползунке
+ //устанавл. нач. и конеч. знач в ползунке, скрываем их с экрана, шаг ползунка 1000000
+ //отслеживаеть радиокнопку в html через data-month="" data-per=""
+ var elem = document.querySelector('.slider'); 
+ var init = new Powerange(elem, {
+   min: 0,
+   max: 9999,
+   klass: '.slider',
+   decimal: true,
+   start: 500,
+   hideRange: true,
+   step: 100 
+  });
