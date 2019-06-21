5684cad0602ee776a2358e8e91e276f643855279
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-21 17:29:21 +0300

    made a slider like in a psd

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-
+/*
 let rng = document.getElementById('myRange');
 let power = document.getElementById('power');
 let cost = document.getElementById('cost');
@@ -6,18 +6,25 @@ let cost = document.getElementById('cost');
  power.value=rng.value;
  cost.value=power.value*3.30;
 }
-
+*/
 /*из библ. Powerange вставляем ползунок*/
- //находим наш класс в ползунке
- //устанавл. нач. и конеч. знач в ползунке, скрываем их с экрана, шаг ползунка 1000000
- //отслеживаеть радиокнопку в html через data-month="" data-per=""
- var elem = document.querySelector('.slider'); 
- var init = new Powerange(elem, {
+ 
+ let elem = document.querySelector('.slider'); 
+ let init = new Powerange(elem, {
    min: 0,
    max: 9999,
-   klass: '.slider',
-   decimal: true,
+   //klass: '',
+   //decimal: true,
    start: 500,
    hideRange: true,
    step: 100 
   });
+
+
+let power = document.getElementById('power');
+let cost = document.getElementById('cost');
+elem.onchange = function() {
+  power.value = elem.value;
+  cost.value=(power.value*3.30).toFixed(2);
+};
+ 
